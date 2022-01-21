eeaea7e799f8deeb248f8a979cfa850cd17fdfc3
JordanVinces <1be.jordan.villarreal@gmail.com>
2022-01-20 19:38:56 -0500

    Agregar archivos de proyecto.

---
+++ Patrones.Singleton.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using Patrones.Singleton.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Singleton.UIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Usuario usuario = new Usuario();
+            usuario.Username = "coco";
+            usuario.Password = "dulce";
+
+            try
+            {
+
+                Sesionadmid.Login(usuario);
+                
+                Sesionadmid u = Sesionadmid.GetInstance;
+
+                Sesionadmid.Logout();
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+
+         
+
+        }
+    }
+}
---
+++ Patrones.Singleton.Core/Sesionadmid.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Singleton.Core
+{
+    public class Sesionadmid
+    {
+
+        private static object _lock = new Object();
+        private static Sesionadmid _session;
+
+        public Usuario Usuario { get; set; }
+        public DateTime FechaInicio { get; set; }
+
+        public static Sesionadmid GetInstance
+        {
+            get
+            {
+                if (_session == null) throw new Exception("Sesión no iniciada");
+
+                return _session;
+            }
+        }
+
+       public static void Login(Usuario usuario)
+        {
+
+            lock (_lock)
+            {
+                if (_session == null)
+                {
+                    _session = new Sesionadmid();
+                    _session.Usuario = usuario;
+                    _session.FechaInicio = DateTime.Now;
+                }
+                else
+                {
+                    throw new Exception("Sesión ya iniciada");
+                }
+            }
+            } 
+
+        public static void Logout()
+        {
+            lock (_lock)
+            {
+                if (_session != null)
+                {
+                    _session = null;
+                }
+                else
+                {
+                    throw new Exception("Sesión no iniciada");
+                }
+            }
+
+           
+        }
+
+        private Sesionadmid()
+        {
+
+        }
+
+
+    }
+}
